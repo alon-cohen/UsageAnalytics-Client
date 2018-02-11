21aadd8cfac69e0a4c80cda73bce42af7b215af3
judokalon <alvani.cohen@gmail.com>
2018-02-11 23:53:28 +0200

    working on dashboard

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent implements OnInit {
+  selectedItem = 'dashboard';
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
