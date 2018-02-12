14eb38103b6081992199af80809f332a5904e499
judokalon <alvani.cohen@gmail.com>
2018-02-12 15:05:40 +0200

    Dashboard Designing is done!

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,10 +8,19 @@ import * as c3 from 'c3';
 })
 export class DashboardComponent implements OnInit {
   top3services;
+  top3veticals;
+  top3extensions;
+  platforms;
+  usersTimeline;
+
   constructor() { }
 
   ngOnInit() {
     this.initTop3Services();
+    this.initTop3Verticals();
+    this.initTop3Extensions();
+    this.initPlatforms();
+    this.initUsersTimeline();
   }
 
   initTop3Services() {
@@ -41,6 +50,11 @@ export class DashboardComponent implements OnInit {
         space: 0.3
       },
       axis: {
+        x: {
+          tick: {
+            format: function (x) { return '';};
+          }
+        },
         y: {
           tick: {
               format: function (d) {
@@ -49,15 +63,176 @@ export class DashboardComponent implements OnInit {
           }
         }
       },
-      grid: {
-        y: {
+      tooltip: {
+        grouped: false,
+      }
+    });
+  }
 
+  initTop3Verticals() {
+    this.top3veticals = c3.generate({
+      bindto: '#top3verticals',
+      size: {
+        width: 360,
+        height: 265
+      },
+      color: {
+        pattern: ['#4ed8da'],
+      },
+      padding: {
+        top: 13,
+        left: 50
+      },
+      data: {
+        x : 'x',
+        columns: [
+          ['x', '43400 - DX Security package', '44202 - Self-Monitoring, Video, Automat…', '44204 - Security, Video, Automation an...'],
+          ['Total', 18, 32, 27],
+        ],
+        type: 'bar'
+      },
+      bar: {
+        width: 60,
+      },
+      axis: {
+        x: {
+          type: 'category',
+          tick: {
+            multiline: true
+          },
+        },
+        y: {
+          tick: {
+            format: function (d) { return d + '%'; }
+          }
         }
       },
       tooltip: {
         grouped: false,
       },
+      legend: {
+        hide: true
+      }
+    });
+  }
 
+  initTop3Extensions() {
+    this.top3extensions = c3.generate({
+      bindto: '#top3extensions',
+      size: {
+        width: 360,
+        height: 265
+      },
+      color: {
+        pattern: ['#b9f6f7'],
+      },
+      padding: {
+        top: 13,
+        left: 50
+      },
+      data: {
+        x : 'x',
+        columns: [
+          ['x', '60221 - Security extension w…', '44207 - Energy Extension…', '44206 - Automation Extention'],
+          ['Total', 20, 40, 26],
+        ],
+        type: 'bar'
+      },
+      bar: {
+        width: 60,
+      },
+      axis: {
+        x: {
+          type: 'category',
+          tick: {
+            multiline: true
+          },
+        },
+        y: {
+          tick: {
+            format: function (d) { return d + '%'; }
+          }
+        }
+      },
+      tooltip: {
+        grouped: false,
+      },
+      legend: {
+        hide: true
+      }
+    });
+  }
+
+  initPlatforms() {
+    this.platforms = c3.generate({
+      bindto: '#platforms',
+      size: {
+        width: 360,
+        height: 265
+      },
+      color: {
+        pattern: ['#4ed8da', '#c04dd8', '#e06950', '#f8e71c'],
+      },
+      // padding: {
+      //   top: 13,
+      //   left: 50
+      // },
+      data: {
+        columns: [
+          ['Web', 26],
+          ['Mobile', 24],
+          ['IFTTT', 32.5],
+          ['Alexa', 17.5],
+        ],
+        type : 'donut',
+        onclick: function (d, i) { console.log('onclick', d, i); },
+        onmouseover: function (d, i) { console.log('onmouseover', d, i); },
+        onmouseout: function (d, i) { console.log('onmouseout', d, i); }
+      },
+      donut: {
+        title: 'All Services Usage'
+      },
+      legend: {
+        position: 'right'
+      }
+    });
+  }
+
+  initUsersTimeline() {
+    this.usersTimeline = c3.generate({
+      bindto: '#users-timeline',
+      size: {
+        width: 740,
+        height: 270
+      },
+      color: {
+        pattern: ['#4ed8da'],
+      },
+      padding: {
+        top: 15,
+        left: 50
+      },
+      data: {
+        x: 'x',
+        columns: [
+          ['x', '2017-06-01', '2017-07-01', '2017-08-01', '2017-09-01', '2017-10-01', '2017-11-01', '2017-12-01'],
+          ['data1', 250, 520, 1250, 2550, 3000, 3200, 3215]
+        ]
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%Y-%m-%d'
+          }
+        }
+      },
+      zoom: {
+        enabled: true
+      },
+      legend: {
+        show: 'false',
+      }
     });
   }
 }
